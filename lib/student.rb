7f7ed42964a454a6cb2122760cafa43cf5c04834
Kitty Davies <kittyfdavies@gmail.com>
2018-10-10 01:50:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array each do |student|
+    students_array.each do |student|
       student.new
     end
     binding.pry
